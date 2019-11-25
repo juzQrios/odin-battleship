9d3e5ad7fcf5806d98eedfd3cf2f686d8d246b11
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-11-25 18:56:32 +0200

    add html and css extract packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
+import './css/reset.css';
+import './css/main.css';
+
 import DOM from './modules/DOM';
 import Game from './modules/Game';
+
 
 DOM.renderBoards();
 Game.initGame();
 // Game.startGame();
+
+// wait for player move => bind each cells with the playturn (getPlayerCoordinates)
+const nonamemethod = (x, y) => {
+  Game.startTurn(x, y);
+  DOM.renderBoards();
+};
+
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell, index) => {
+  cell.addEventListener('click', () => nonamemethod(Math.ceil(index / 10), index % 10));
+});
+
+// check if the point is valid
+// play move
+// computer playMove
+// end of loop: 1. remove bind events
+//              2. check if winning position reached
+
+// wait for player move => bind each cells with the playturn (getPlayerCoordinates)
+// check if the point is valid
+// play move
+// computer playMove
+// end of loop: 1. remove bind events
+//              2. check if winning position reached
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,9 +8,26 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3000
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loader: [MiniCssExtractPlugin.loader, 'css-loader']
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    }),
+  ]
 };
