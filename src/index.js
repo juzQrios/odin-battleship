466e181788a5ef45332554af2b761bc5f517562b
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-11-26 18:13:42 +0200

    Add ui ship placing logic

--- src/index.js
+++ src/index.js
@@ -7,29 +7,39 @@ import Game from './modules/Game';
 
 DOM.renderBoards();
 Game.initGame();
-// Game.startGame();
+// place ships logic
 
-// wait for player move => bind each cells with the playturn (getPlayerCoordinates)
-const nonamemethod = (x, y) => {
-  Game.startTurn(x, y);
-  DOM.renderBoards();
+const shipsLength = [2, 3, 3, 4, 5];
+const placeAllShips = async () => {
+  for (let index = 0; index < shipsLength.length; index += 1) {
+    const startPoint = await DOM.getUserInput();
+    const endPoint =  await DOM.getUserInput();
+
+    console.log("start point is: " + startPoint);
+    console.log("end Point is: " + endPoint);
+
+    const orientation = startPoint[0] === endPoint[0] ? 'h' : 'v';
+    Game.computerBoard.placeShip(shipsLength[index], orientation, ...startPoint);
+    DOM.renderBoards();
+    // if (Game.computerBoard.validate(i, j, orientation)) {
+    //   console.log('validated');
+    //   Game.computerBoard.placeShip(shipsLength[index], orientation, i[0], i[1]);
+    //   DOM.renderBoards();
+    // } else {
+    //   console.log('cells taken');
+    //   // TODO: show message that the cells are taken
+    // }
+  }
 };
+placeAllShips();
 
+// start the game
+// const gameLoop = async () => {
+//   while (1) {
+//     const clickedCoordinates = await DOM.getUserInput();
+//     Game.startTurn(...clickedCoordinates);
+//     DOM.renderBoards();
+//   }
+// };
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell, index) => {
-  cell.addEventListener('click', () => nonamemethod(Math.ceil(index / 10), index % 10));
-});
-
-// check if the point is valid
-// play move
-// computer playMove
-// end of loop: 1. remove bind events
-//              2. check if winning position reached
-
-// wait for player move => bind each cells with the playturn (getPlayerCoordinates)
-// check if the point is valid
-// play move
-// computer playMove
-// end of loop: 1. remove bind events
-//              2. check if winning position reached
+// gameLoop();
